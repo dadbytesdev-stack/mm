72687ac95b02c379c94ee81161ae1cd5f7d8afa8
dadbytesdev-stack <dadbytesdev@gmail.com>
2026-02-09 23:12:52 -0800

    config update

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
+  reloadOnOnline: false,
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  swcMinify: true,
 });
