003e5e780c7fe65d659a033d16ad088c8645acbb
Mathew Smith <dadbytes@Mathews-MBP.localdomain>
2026-05-02 22:46:56 -0700

    Fix native app detection via WKUserScript

--- MindMantra/ViewController.swift
+++ MindMantra/ViewController.swift
@@ -1,14 +1,26 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
     
     var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let contentController = WKUserContentController()
+        
+        // Inject JavaScript to set a flag that this is the native app
+        let script = WKUserScript(
+            source: "window.isNativeApp = true;",
+            injectionTime: .atDocumentStart,
+            forMainFrameOnly: true
+        )
+        contentController.addUserScript(script)
+        contentController.add(self, name: "nativeApp")
+        
         let config = WKWebViewConfiguration()
+        config.userContentController = contentController
         config.allowsInlineMediaPlayback = true
         
         webView = WKWebView(frame: view.bounds, configuration: config)
@@ -20,5 +32,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let request = URLRequest(url: url)
         webView.load(request)
     }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // Handle messages from JavaScript if needed
+    }
 }
-
